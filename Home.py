46d37fb2eab0d91910d5efe153af53b19a251ce6
CoderKane <michelbritain@gmail.com>
2023-10-21 20:12:59 -0500

    Milestone 5 initial delivery (WIP)

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 import os
+import json
+from pathlib import Path
 import streamlit as st
+from streamlit_card import card
 
 st.set_page_config(
     page_title="AI Chatbot 🤖",
@@ -8,12 +11,25 @@ st.set_page_config(
 
 st.title("AI Chatbot")
 
+# load the topics from the json file
+with open(Path("topics.json"), "r") as f:
+    topics = json.load(f)
+
 # sidebar
-st.sidebar.write("Select topic")
-st.sidebar.info("Topic 1")
-st.sidebar.info("Topic 2")
-st.sidebar.info("Topic 3")
-st.sidebar.info("Topic 4")
+st.sidebar.write("Or, select a topic from the list below:")
+if st.sidebar.button(topics["sidebar_topic_1"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_1"]
+if st.sidebar.button(topics["sidebar_topic_2"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_2"]
+if st.sidebar.button(topics["sidebar_topic_3"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_3"]
+if st.sidebar.button(topics["sidebar_topic_4"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_4"]
+if st.sidebar.button(topics["sidebar_topic_5"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_5"]
+if st.sidebar.button(topics["sidebar_topic_6"],type="primary"):
+    prompt_to_start = topics["sidebar_topic_6"]
+
 
 from langchain.schema import HumanMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
@@ -31,7 +47,7 @@ load_dotenv()
 
 #define llm to use
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-system_prompt = "Hi, I'm an AI assistant. What would you like to ask me?"
+system_prompt = f"""I am an AI assistant, ask me anything!"""
 
 
 # function that loads the vectorstore from the local directory
@@ -40,23 +56,33 @@ def load_vectorstore():
     return vectorstore
 
 def create_response(vectorstore,query):
-    condense_question_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language. 
-    Also add semantic keywords associated with the question. For example, if the question is "who is suing who?", the semantic keywords could be "plaintiff", "defendant", etc. add these keywords to the end of the question, separated by commas.
+    # condense_question_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language. 
+    # Also add semantic keywords associated with the question. For example, if the question is "who is suing who?", the semantic keywords could be "plaintiff", "defendant", etc. add these keywords to the end of the question, separated by commas.
+    # If you don't know the answer, answer with your own best guess.
+    
+    # Chat History:
+    # {chat_history}
+    # Follow Up Input: {question}
+    # Standalone question:"""
 
-    Chat History:
-    {chat_history}
-    Follow Up Input: {question}
-    Standalone question:"""
+    # CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question_template)
 
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question_template)
+    # qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever = vectorstore.as_retriever(), memory=st.session_state['memory'], return_source_documents=True, condense_question_prompt=CONDENSE_QUESTION_PROMPT)
+    # response = qa_chain({"question": query})
 
-    qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever = vectorstore.as_retriever(), memory=st.session_state['memory'], return_source_documents=True, condense_question_prompt=CONDENSE_QUESTION_PROMPT)
-    response = qa_chain({"question": query})
+    # add the user message to the memory
+    st.session_state['memory'].chat_memory.add_user_message(query)
+
+    # generate a response using the history
+    chat = ChatOpenAI(model='gpt-3.5-turbo-16k')
+    messages = st.session_state['memory'].chat_memory.messages
+    new_msg = chat.invoke(messages).content
+    st.session_state['memory'].chat_memory.add_ai_message(new_msg)
 
     #debug, st.write the source documents
     # st.write(response["source_documents"])
 
-    return response["answer"]
+    return new_msg
 
 with st.spinner('Initializing memory...'):
     # set the streamlit memory if not already set
@@ -75,8 +101,84 @@ with st.spinner('Initializing vectorstore...'):
 # warning to tell user to clear vector store if new documents were added
 # st.warning('If you have added new documents, please rebuild the vectorstore by clicking the button above.')
 
-# placeholder for the chat messages
-chat_messages_ph = st.empty()
+cc = st.columns(3)
+
+prompt_to_start = None
+def setPromptToStart(prompt):
+    global prompt_to_start
+    prompt_to_start = prompt
+
+theme_card = {'bgcolor': '#FFF0F0','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
+card_style = {"card": {
+            "width": "160px",
+            "height": "100px",
+            "border-radius": "5px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            "margin": "1px",
+        },"text": {
+            "font-size": "1.0rem",
+        }}
+
+with cc[0]:
+    # hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral', key="card1", theme_override=theme_card, content_text_size="1.0rem", title_text_size="1.0rem")
+    card_topic_1_clicked = card(
+        title= topics["card_topic_1"],
+        text="e",
+        url="",
+        key="card1",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart(topics["card_topic_1"])
+    )
+    card_topic_4_clicked = card(
+        title=topics["card_topic_4"],
+        text="",
+        key="card2",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart()
+    )
+with cc[1]:
+    # hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral', key="card2", theme_override=theme_card, content_text_size="1.0rem", title_text_size="1.0rem")
+    card_topic_2_clicked = card(
+        title= topics["card_topic_2"],
+        text="",
+        key="card3",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart()
+    )
+    card_topic_5_clicked = card(
+        title=topics["card_topic_5"],
+        text="",
+        key="card4",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart()
+    )
+with cc[2]:
+    # hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral', key="card3", theme_override=theme_card, content_text_size="1.0rem", title_text_size="1.0rem")
+    card_topic_3_clicked = card(
+        title=topics["card_topic_3"],
+        text="",
+        key="card5",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart()
+    )
+    card_topic_6_clicked = card(
+        title=topics["card_topic_6"],
+        text="",
+        key="card6",
+        image="",
+        styles=card_style,
+        on_click=lambda: setPromptToStart()
+    )
+
+# display radio buttons to choose psychologist
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
+st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
+psychologist_selected = st.radio("Choose your Psychologist:",("Dr. Huang","Dr. Wong","Ms. Amy"))
 
 #display the chat input
 with st.container():
@@ -86,8 +188,13 @@ with st.container():
         create_response(vectorstore, prompt)
         # st.session_state['memory'].chat_memory.add_user_message(prompt)
 
+# create response if prompt_to_start variable exists
+if prompt_to_start is not "":
+    create_response(vectorstore, prompt_to_start)
+    del prompt_to_start
+
 # add a speech to text button
-audio_bytes = audio_recorder(sample_rate=44100,key="voice")
+audio_bytes = audio_recorder(text="Click to speak!", sample_rate=44100,key="voice")
 if audio_bytes:
     # random file name
     import uuid 
@@ -128,4 +235,4 @@ for index, message in enumerate(st.session_state['memory'].load_memory_variables
 if st.button('Clear Chat'):
     st.session_state['memory'].chat_memory.clear()
     st.session_state['memory'].chat_memory.add_ai_message(system_prompt)
-    st.experimental_rerun()
+    st.rerun()
--- pages/3_🗂️_Topics_Setup.py
+++ pages/3_🗂️_Topics_Setup.py
@@ -1,10 +1,51 @@
 import streamlit as st
 from pathlib import Path
 from password import check_password
+import json
 
-st.header("Not implemented yet")
-st.markdown("This feature is not implemented yet. Please check back later.")
+st.header("Topics Setup")
+st.markdown("This page allows you to set up the topics for the cards and the side-bar.")
 
 if not check_password():
     st.stop()
 
+# prompt the user to enter the topics for the cards
+st.markdown("Enter the topics for the cards below")
+card_topic_1 = st.text_input("Topic 1")
+card_topic_2 = st.text_input("Topic 2")
+card_topic_3 = st.text_input("Topic 3")
+card_topic_4 = st.text_input("Topic 4")
+card_topic_5 = st.text_input("Topic 5")
+card_topic_6 = st.text_input("Topic 6")
+
+# prompt the user to enter the questions for the side-bar
+st.markdown("Enter the topics for the side-bar below")
+sidebar_topic_1 = st.text_input("Sidebar Topic 1")
+sidebar_topic_2 = st.text_input("Sidebar Topic 2")
+sidebar_topic_3 = st.text_input("Sidebar Topic 3")
+sidebar_topic_4 = st.text_input("Sidebar Topic 4")
+sidebar_topic_5 = st.text_input("Sidebar Topic 5")
+sidebar_topic_6 = st.text_input("Sidebar Topic 6")
+
+
+# put everything into a dictionary
+topics = {
+    "card_topic_1": card_topic_1,
+    "card_topic_2": card_topic_2,
+    "card_topic_3": card_topic_3,
+    "card_topic_4": card_topic_4,
+    "card_topic_5": card_topic_5,
+    "card_topic_6": card_topic_6,
+    "sidebar_topic_1": sidebar_topic_1,
+    "sidebar_topic_2": sidebar_topic_2,
+    "sidebar_topic_3": sidebar_topic_3,
+    "sidebar_topic_4": sidebar_topic_4,
+    "sidebar_topic_5": sidebar_topic_5,
+    "sidebar_topic_6": sidebar_topic_6,
+}
+
+# dump it to a local json file, when user clicks button
+if st.button("Save"):
+    with open(Path("topics.json"), "w") as f:
+        json.dump(topics, f)
+    st.success("Topics saved successfully")
