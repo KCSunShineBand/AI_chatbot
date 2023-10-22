df5a80cf57d409f553507979666cc4926ae1b43a
CoderKane <michelbritain@gmail.com>
2023-10-22 15:05:02 -0500

    Finalize milestone 5

--- Home.py
+++ Home.py
@@ -15,22 +15,6 @@ st.title("AI Chatbot")
 with open(Path("topics.json"), "r") as f:
     topics = json.load(f)
 
-# sidebar
-st.sidebar.write("Or, select a topic from the list below:")
-if st.sidebar.button(topics["sidebar_topic_1"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_1"]
-if st.sidebar.button(topics["sidebar_topic_2"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_2"]
-if st.sidebar.button(topics["sidebar_topic_3"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_3"]
-if st.sidebar.button(topics["sidebar_topic_4"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_4"]
-if st.sidebar.button(topics["sidebar_topic_5"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_5"]
-if st.sidebar.button(topics["sidebar_topic_6"],type="primary"):
-    prompt_to_start = topics["sidebar_topic_6"]
-
-
 from langchain.schema import HumanMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
@@ -55,7 +39,7 @@ def load_vectorstore():
     vectorstore = Chroma(persist_directory="db_document_embeddings",embedding_function=OpenAIEmbeddings())
     return vectorstore
 
-def create_response(vectorstore,query):
+def create_response(vectorstore):
     # condense_question_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language. 
     # Also add semantic keywords associated with the question. For example, if the question is "who is suing who?", the semantic keywords could be "plaintiff", "defendant", etc. add these keywords to the end of the question, separated by commas.
     # If you don't know the answer, answer with your own best guess.
@@ -69,10 +53,6 @@ def create_response(vectorstore,query):
 
     # qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever = vectorstore.as_retriever(), memory=st.session_state['memory'], return_source_documents=True, condense_question_prompt=CONDENSE_QUESTION_PROMPT)
     # response = qa_chain({"question": query})
-
-    # add the user message to the memory
-    st.session_state['memory'].chat_memory.add_user_message(query)
-
     # generate a response using the history
     chat = ChatOpenAI(model='gpt-3.5-turbo-16k')
     messages = st.session_state['memory'].chat_memory.messages
@@ -103,10 +83,35 @@ with st.spinner('Initializing vectorstore...'):
 
 cc = st.columns(3)
 
-prompt_to_start = None
+# check if already_prompted is set, if not set it to false
+if 'already_prompted' not in st.session_state:
+    st.session_state['already_prompted'] = ""
+
+prompt_to_start = ""
 def setPromptToStart(prompt):
     global prompt_to_start
     prompt_to_start = prompt
+
+    # if the previous prompt is the same as the current prompt, then set it to empty
+    if st.session_state['already_prompted'] == prompt:
+        prompt_to_start = ""
+    st.session_state['already_prompted'] = prompt
+
+# sidebar
+st.sidebar.write("Or, select a topic from the list below:")
+if st.sidebar.button(topics["sidebar_topic_1"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_1"])
+if st.sidebar.button(topics["sidebar_topic_2"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_2"])
+if st.sidebar.button(topics["sidebar_topic_3"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_3"])
+if st.sidebar.button(topics["sidebar_topic_4"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_4"])
+if st.sidebar.button(topics["sidebar_topic_5"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_5"])
+if st.sidebar.button(topics["sidebar_topic_6"],type="primary"):
+    setPromptToStart(topics["sidebar_topic_6"])
+
 
 theme_card = {'bgcolor': '#FFF0F0','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
 card_style = {"card": {
@@ -124,7 +129,6 @@ with cc[0]:
     card_topic_1_clicked = card(
         title= topics["card_topic_1"],
         text="e",
-        url="",
         key="card1",
         image="",
         styles=card_style,
@@ -136,7 +140,7 @@ with cc[0]:
         key="card2",
         image="",
         styles=card_style,
-        on_click=lambda: setPromptToStart()
+        on_click=lambda: setPromptToStart(topics["card_topic_4"])
     )
 with cc[1]:
     # hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral', key="card2", theme_override=theme_card, content_text_size="1.0rem", title_text_size="1.0rem")
@@ -146,7 +150,7 @@ with cc[1]:
         key="card3",
         image="",
         styles=card_style,
-        on_click=lambda: setPromptToStart()
+        on_click=lambda: setPromptToStart(topics["card_topic_2"])
     )
     card_topic_5_clicked = card(
         title=topics["card_topic_5"],
@@ -154,7 +158,7 @@ with cc[1]:
         key="card4",
         image="",
         styles=card_style,
-        on_click=lambda: setPromptToStart()
+        on_click=lambda: setPromptToStart(topics["card_topic_5"])
     )
 with cc[2]:
     # hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral', key="card3", theme_override=theme_card, content_text_size="1.0rem", title_text_size="1.0rem")
@@ -164,7 +168,7 @@ with cc[2]:
         key="card5",
         image="",
         styles=card_style,
-        on_click=lambda: setPromptToStart()
+        on_click=lambda: setPromptToStart(topics["card_topic_3"])
     )
     card_topic_6_clicked = card(
         title=topics["card_topic_6"],
@@ -172,7 +176,7 @@ with cc[2]:
         key="card6",
         image="",
         styles=card_style,
-        on_click=lambda: setPromptToStart()
+        on_click=lambda: setPromptToStart(topics["card_topic_6"])
     )
 
 # display radio buttons to choose psychologist
@@ -180,20 +184,8 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content
 st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
 psychologist_selected = st.radio("Choose your Psychologist:",("Dr. Huang","Dr. Wong","Ms. Amy"))
 
-#display the chat input
-with st.container():
-    prompt = st.chat_input("Say something")
-    if prompt:
-        # create the response
-        create_response(vectorstore, prompt)
-        # st.session_state['memory'].chat_memory.add_user_message(prompt)
-
-# create response if prompt_to_start variable exists
-if prompt_to_start is not "":
-    create_response(vectorstore, prompt_to_start)
-    del prompt_to_start
-
 # add a speech to text button
+prompt_audio = ""
 audio_bytes = audio_recorder(text="Click to speak!", sample_rate=44100,key="voice")
 if audio_bytes:
     # random file name
@@ -215,24 +207,59 @@ if audio_bytes:
     os.remove(file_name)
 
     # generate the response
-    response = create_response(vectorstore, transcript.text)
+    prompt_audio = transcript.text
 
     # clear audio bytes
     if 'voice' in st.session_state:
          del st.session_state['voice']
 
-for index, message in enumerate(st.session_state['memory'].load_memory_variables({})['chat_history']):
-    if isinstance(message, HumanMessage):      
-        # with ph:             
-            bot_msg = st.chat_message("assistant")
-            bot_msg.write(message.content)
-    elif isinstance(message, AIMessage):
-        # with ph: 
-            human_msg = st.chat_message("user")
-            human_msg.write(message.content)
+def populate_chat():
+    # array to hold chat messages
+    chat_messages = []
+
+    # chat template
+    for index, message in enumerate(st.session_state['memory'].load_memory_variables({})['chat_history']):
+
+        if isinstance(message, HumanMessage):      
+            # with ph:             
+                human_msg = st.chat_message("human")
+                human_msg.write(message.content)
+                # add the message to the chat messages array
+                chat_messages.append(human_msg)
+        elif isinstance(message, AIMessage):
+            # with ph: 
+                ai_msg = st.chat_message("ai")
+                ai_msg.write(message.content)
+                chat_messages.append(ai_msg)
+
+    return chat_messages
 
 # clear chat button
 if st.button('Clear Chat'):
     st.session_state['memory'].chat_memory.clear()
     st.session_state['memory'].chat_memory.add_ai_message(system_prompt)
-    st.rerun()
+
+# input
+prompt = st.chat_input("Say something")
+if prompt or prompt_to_start != "" or prompt_audio != "":
+    # check which prompt to use
+    if prompt_to_start:
+        prompt = prompt_to_start
+        prompt_to_start = ""
+
+    # check if audio was used
+    if prompt_audio:
+        prompt = prompt_audio
+        prompt_audio = ""
+
+    # add message to history
+    st.session_state['memory'].chat_memory.add_user_message(prompt)
+
+    # populate the chat
+    populate_chat()
+    with st.spinner("Waiting for response..."):
+        new_message = create_response(vectorstore)
+        ai_msg = st.chat_message("ai")
+        ai_msg.write(new_message)
+else:
+    populate_chat()
